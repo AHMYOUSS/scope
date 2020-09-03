d05dcd8c6d4c3a8c70f303cd1e1031e9e1730e62
AHMYOUSS <ahmid.ismail@gmail.com>
2020-09-03 20:05:17 +0200

    first commit

---
+++ Untitle.js
@@ -0,0 +1,44 @@
+/* Convert and print the given time in -hour format, where .
+Sample Input 0
+07:05:45PM
+Sample Output 0
+19:05:45 */
+function timeConversion() {
+    var today = new Date();
+  var day = today.getDay();
+  var hour = today.getHours();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
+  var prepand = (hour >= 12)? " PM ":" AM ";
+  hour = (hour >= 12)? hour - 12: hour;
+  if (hour===0 && prepand===' PM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  prepand=' Noon';
+  } 
+  else
+  { 
+  hour=12;
+  prepand=' PM';
+  } 
+  } 
+  if (hour===0 && prepand===' AM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  prepand=' Midnight';
+  } 
+  else
+  { 
+  hour=12;
+  prepand=' AM';
+  } 
+  } 
+console.log("Current Time : "+hour + prepand + " : " + minute + " : " + second);
+if ( hours >= 12 ) { hour = hour + 12 }
+return console.log(hour)
+}
+ timeConversion()
